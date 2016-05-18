10901033b35954dd9643f975cfa09916d3a3c6cc
Bhaskar <bhaskar.anil429@gmail.com>
2016-05-18 13:46:31 -0500

    Create MCP23008.py

---
+++ MCP23008.py
@@ -0,0 +1,20 @@
+from OmegaExpansion import onionI2C
+import time
+import sys
+print 'Starting: onionI2C module testing...'
+i2c     = onionI2C.OnionI2C(0)
+# set the verbosity
+i2c.setVerbosity(1)
+value = [0x00]
+data = i2c.writeBytes(0x21, 0x00, value)
+while True:
+        time.sleep(0.5)
+        value1 = [0xFF]
+        data = i2c.writeBytes(0x21, 0x09, value1)
+        print "Turning all Relays ON"
+        time.sleep(1)
+        data = i2c.writeBytes(0x21, 0x09, value)
+        print "Turning all Relays OFF
+        time.sleep(1)
+        print "\n"
+
